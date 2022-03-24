9668b7848e9d66279fe0682da2ff47e333b93637
maderero <maderero@github.com>
2022-03-24 16:53:58 -0500

    added example loads

--- Examples/Braced Frame - Repair Usage.py
+++ Examples/Braced Frame - Repair Usage.py
@@ -17,7 +17,7 @@ count_verticals = 10
 strong_params = {'E': 999999, 'G': 100, 'Iy': 1000, 'Iz': 1000, 'J': 100, 'A': 100}
 
 # Add Horizontals
-releases_horizontals = {} #'Ryi': True, 'Rzi': True, 'Ryj': True, 'Rzj': True, }
+releases_horizontals = {'Ryi': True, 'Rzi': True, 'Ryj': True, 'Rzj': True, }
 names_horizontals = []
 for i in range(count_horizontals):
     y = i / (count_horizontals - 1) * length_verticals
@@ -39,6 +39,7 @@ for i in range(count_verticals):
 # Add and define supports
 support_node_left = f.find_node_by_coordinates((0, 0, 0))
 support_node_right = f.find_node_by_coordinates((length_horizontals, 0, 0))
+
 f.def_support(
     support_node_left,
     support_DX=True, support_DY=True, support_DZ=True,
@@ -48,6 +49,39 @@ f.def_support(
     support_DX=True, support_DY=True, support_DZ=True,
     support_RX=True, support_RY=True, support_RZ=True)
 
+# Add a distributed load. Should be on the horizontal closest to y=0.
+member = list(f.Members.values())[0]
+f.add_member_dist_load(
+    Member=member.name,
+    Direction='Fz',
+    w1=0.2, w2=0.8,
+    x1=0.2, x2=0.8*member.L())
+
+# Add a distributed load. Should be the vertical closest to x=0.
+member = list(f.Members.values())[count_horizontals]
+f.add_member_dist_load(
+    Member=member.name,
+    Direction='Fz',
+    w1=0.3, w2=0.7,
+    x1=0.3, x2=0.7*member.L())
+
+
+# Add a member point load. Should be on the horizontal furthest from y=0.
+member = list(f.Members.values())[count_horizontals-1]
+f.add_member_pt_load(
+    Member=member.name,
+    Direction='Fz',
+    P=3.14, x=0.25*member.L(),
+    case='Case 1')
+
+# Add a member point load. Should be j node of the vertical furthest from x=0.
+member = list(f.Members.values())[-1]
+f.add_member_pt_load(
+    Member=member.name,
+    Direction='Fz',
+    P=1.57, x=0.25*member.L(),
+    case='Case 1')
+
 # Clean-up
 f.repair(
     merge_duplicates=True,  # must be run to avoid duplicate nodes
@@ -55,7 +89,7 @@ f.repair(
     tolerance_intersection=1e-3)  # maximum 3d distance between lines to determine member intersection
 
 # Analyze
-f.analyze(log=False, check_statics=False)
+f.analyze(log=False, check_statics=True)
 theoretical_members = 2 * count_horizontals * count_verticals - count_horizontals - count_verticals
 actual_members = len(f.Members)
 
